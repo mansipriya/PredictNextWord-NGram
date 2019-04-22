24cfbe15b8c18e1b86b5ba88c5aca4b24db9f774
mansipriya <mail4mansipriya@gmail.com>
2019-04-22 14:08:50 +0530

    Final Program

---
+++ getTopBigram.py
@@ -0,0 +1,36 @@
+import pickle
+from collections import OrderedDict
+import heapq # for getting top 5
+
+pickleDumps = "pickleDumps/"
+conditionalProbabilityFile = pickleDumps+"conditionalProbabilityDict.p"
+bigramsListPath = pickleDumps + "bigramsList.p"
+
+file = open(conditionalProbabilityFile,"rb")
+conditionalProbabilityDict = pickle.load(file)
+
+file = open(bigramsListPath,"rb")
+bigramsList = pickle.load(file)
+
+while True:
+	checkForThisBigram = input("Enter a word to predict its next probable words ('endthis' for stopping) : ")
+
+	if checkForThisBigram == "endthis":
+		break;
+
+	# empty the list, for new iteration
+	matchedBigrams = [] # all bigrams that starts with the inputted word
+	for bigram in bigramsList:
+		if checkForThisBigram == bigram[1]:
+			matchedBigrams.append(bigram[1]+" "+bigram[2])
+
+	# print matchedBigrams
+	topDict = {}
+	for singleBigram in matchedBigrams:
+		topDict[singleBigram] = conditionalProbabilityDict[singleBigram]
+
+	topBigrams = heapq.nlargest(5, topDict, key=topDict.get)
+	for b in topBigrams:
+		 print (b + " : " + str(topDict[b]) + "\n")
+
+	print ("\n" + "____________________" + "\n")
